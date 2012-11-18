a93baee5aa4f19113d9593daf49f54be083c8d1f
Derik Whittaker <derik@graudo.com>
2012-11-18 12:54:33 -0500

    Chapter 6 Lesson 5

---
+++ Chapter.6.WorkingWithTiles/Lesson.5.ToastNotifications/LL.ToastNotifications/LL.ToastNotifications/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Metro.LL.Common;
+
+namespace LL.ToastNotifications.ViewModels
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Toast Notifications";
+        }
+    }
+}
---
+++ Chapter.6.WorkingWithTiles/Lesson.5.ToastNotifications/LL.ToastNotifications/LL.ToastNotifications/Views/DashboardPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LL.ToastNotifications.ViewModels;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace LL.ToastNotifications.Views
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class DashboardPage : LL.ToastNotifications.Common.LayoutAwarePage
+    {
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new DashboardViewModel();
+        }
+
+    }
+}
