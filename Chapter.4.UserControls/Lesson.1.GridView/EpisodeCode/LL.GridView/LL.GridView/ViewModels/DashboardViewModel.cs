ce1ec3202818e1791c5f4dea3c5afa10b95e8692
Derik Whittaker <derik@graudo.com>
2012-11-19 04:32:11 -0500

    Minor addtions

---
+++ Chapter.4.UserControls/Lesson.1.GridView/EpisodeCode/LL.GridView/LL.GridView/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Metro.LL.Common;
+
+namespace LL.GridView.ViewModels
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use the Grid View";
+        }
+    }
+}
