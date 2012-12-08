697b3a121e2ae3c17ee9056e3f1ef1bb64292e09
Derik Whittaker <derik@graudo.com>
2012-12-08 11:24:07 -0500

    Styles Pre-Record State

---
+++ Chapter.4.UserControls/Lesson.4.StylesAndResources/EpisodeCode/LL.StylesResources/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using Metro.LL.Common;
+
+namespace LL.StylesResources.ViewModels
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use the Styles";
+
+        }
+
+    }
+}
---
+++ Chapter.4.UserControls/Lesson.4.StylesAndResources/EpisodeCode/LL.StylesResources/Views/DashboardPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LL.StylesResources.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace LL.StylesResources.Views
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class DashboardPage : LL.StylesResources.Common.LayoutAwarePage
+    {
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new DashboardViewModel();
+        }
+
+    }
+}
