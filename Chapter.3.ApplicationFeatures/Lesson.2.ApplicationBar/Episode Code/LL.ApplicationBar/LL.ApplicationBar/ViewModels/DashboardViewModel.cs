05e36fea651beb40230387c914d08a0131cafe61
Derik Whittaker <derik@graudo.com>
2012-12-13 19:54:41 -0500

    App Bar Pre-Record State

---
+++ Chapter.3.ApplicationFeatures/Lesson.2.ApplicationBar/Episode Code/LL.ApplicationBar/LL.ApplicationBar/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Metro.LL.Common;
+
+namespace LL.ApplicationBar.ViewModels
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use Appliction Bars";
+        }
+
+    }
+}
---
+++ Chapter.3.ApplicationFeatures/Lesson.2.ApplicationBar/Episode Code/LL.ApplicationBar/LL.ApplicationBar/Views/DashboardPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LL.ApplicationBar.ViewModels;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace LL.ApplicationBar.Views
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class DashboardPage : LL.ApplicationBar.Common.LayoutAwarePage
+    {
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new DashboardViewModel();
+        }
+
+    }
+}
