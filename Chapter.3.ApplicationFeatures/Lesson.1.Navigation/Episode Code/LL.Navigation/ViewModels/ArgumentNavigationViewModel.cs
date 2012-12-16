7f25395856ba3be6d7b69ac43d32ad54fc182dd3
Derik Whittaker <derik@graudo.com>
2012-12-16 13:24:06 -0500

    Navigation Pre-Record State

--- Chapter.3.ApplicationFeatures/Lesson.1.Navigation/Episode Code/LL.Navigation/ViewModels/ArgumentNavigationViewModel.cs
+++ Chapter.3.ApplicationFeatures/Lesson.1.Navigation/Episode Code/LL.Navigation/ViewModels/ArgumentNavigationViewModel.cs
@@ -17,20 +17,7 @@ namespace LL.Navigation.ViewModels
 
             _frame.Navigated += HandleNavigated;            
         }
-
-        private void HandleNavigated(object sender, NavigationEventArgs args)
-        {
-            if ((args.NavigationMode == NavigationMode.New || args.NavigationMode == NavigationMode.Forward)
-                && args.SourcePageType == typeof(ArgumentNavigationPage))
-            {
-                PassedArgument = args.Parameter.ToString();
-            }
-            else
-            {
-                _frame.Navigated -= HandleNavigated;
-            }
-        }
-
+        
         public string PassedArgument
         {
             get { return _passedArgument; }
--- Chapter.3.ApplicationFeatures/Lesson.1.Navigation/Episode Code/LL.Navigation/ViewModels/DashboardViewModel.cs
+++ Chapter.3.ApplicationFeatures/Lesson.1.Navigation/Episode Code/LL.Navigation/ViewModels/DashboardViewModel.cs
@@ -9,66 +9,10 @@ namespace LL.Navigation.ViewModels
 {
     public class DashboardViewModel : BaseViewModel
     {
-        private readonly Frame _currentFrame;
-        private RelayCommand _simpleNavigationCommand;
-        private RelayCommand _simpleNavigationWithEventsCommand;
-        private RelayCommand _simpleNavigationWithForwardCommand;
-        private RelayCommand _simpleNavigationWithArgumentsCommand;
-
-        public DashboardViewModel( Frame currentFrame  )
+        
+        public DashboardViewModel()
         {
-            _currentFrame = currentFrame;
             PageTitle = "Learning to Navigate";
-        }
-
-        public RelayCommand SimpleNavigationCommand
-        {
-            get { return _simpleNavigationCommand ?? (_simpleNavigationCommand = new RelayCommand(SimpleNavigation)); }
-        }
-        
-        private void SimpleNavigation()
-        {
-            _currentFrame.Navigate(typeof (SimpleNavigationPage));
-        }
-
-        public RelayCommand SimpleNavigationWithForwardCommand
-        {
-            get
-            {
-                return _simpleNavigationWithForwardCommand ??
-                       (_simpleNavigationWithForwardCommand = new RelayCommand(SimpleNavigationWithForward));
-            }
-        }
-
-        private void SimpleNavigationWithForward()
-        {
-            if ( _currentFrame.CanGoForward )
-            {
-                _currentFrame.GoForward();
-            }
-        }
-        
-        public RelayCommand SimpleNavigationWithEventsCommand
-        {
-            get { return _simpleNavigationWithEventsCommand ?? (_simpleNavigationWithEventsCommand = new RelayCommand(SimpleNavigationWithEvents)); }
-        }
-
-        private void SimpleNavigationWithEvents()
-        {
-            _currentFrame.Navigated += (sender, args) => { Debug.WriteLine("Navigation Completed"); };
-            _currentFrame.Navigating += (sender, args) => { Debug.WriteLine("Navigation In Progress"); };
-            
-            _currentFrame.Navigate(typeof(SimpleNavigationPage));
-        }
-
-        public RelayCommand SimpleNavigationWithArgumentsCommand
-        {
-            get { return _simpleNavigationWithArgumentsCommand ?? (_simpleNavigationWithArgumentsCommand = new RelayCommand(SimpleNavigationWithArguments)); }
-        }
-
-        private void SimpleNavigationWithArguments()
-        {
-            _currentFrame.Navigate(typeof(ArgumentNavigationPage), "Some Value");
         }
     }
 }
