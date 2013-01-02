141e90d6316419db494a0f578182580de013aa0b
Derik Whittaker <derik@graudo.com>
2013-01-02 17:41:42 -0500

    starting to work on ViewState

---
+++ Chapter.3.ApplicationFeatures/Lesson.4.ApplicationViewState/Episode Code/LL.ViewState/LL.ViewState/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LL.ViewState.ViewModels
+{
+    public class DashboardViewModel : Metro.LL.Common.BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "View State";
+        }
+    }
+}
