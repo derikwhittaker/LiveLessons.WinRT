1582d6869b3050f515e1220ddf8a271a39ceeea5
Derik Whittaker <derik@graudo.com>
2013-01-11 17:49:08 -0500

    Shell of Settings

---
+++ Chapter.5.Contracts/Lesson.4.SettingsContract/Episode Code/LL.SettingsContract/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Metro.LL.Common;
+
+namespace LL.SettingsContract.ViewModels
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use Settings Contract";
+        }
+
+    }
+}
---
+++ Chapter.5.Contracts/Lesson.4.SettingsContract/Episode Code/LL.SettingsContract/Views/DashboardPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LL.SettingsContract.ViewModels;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace LL.SettingsContract.Views
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class DashboardPage : LL.SettingsContract.Common.LayoutAwarePage
+    {
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new DashboardViewModel();
+        }
+
+    }
+}
