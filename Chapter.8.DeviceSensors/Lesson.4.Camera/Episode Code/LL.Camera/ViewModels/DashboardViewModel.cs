21e4a873b2b5be13beccdb07d4aee0ed775a2b76
Derik Whittaker <derik@graudo.com>
2013-01-11 20:37:14 -0500

    Shell for Camera

---
+++ Chapter.8.DeviceSensors/Lesson.4.Camera/Episode Code/LL.Camera/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LL.Camera.ViewModels
+{
+    public class DashboardViewModel : Metro.LL.Common.BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use Camera";
+        }
+    }
+}
