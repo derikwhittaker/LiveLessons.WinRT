5a222054675eab111ae4c8cc09de36573f00f1f4
Derik Whittaker <derik@graudo.com>
2013-01-12 07:27:42 -0500

    WIP for light sensor

--- Chapter.8.DeviceSensors/Lesson.3.LightSensor/Episode Code/LL.LightSensor/LL.LightSensor/ViewModels/DashboardViewModel.cs
+++ Chapter.8.DeviceSensors/Lesson.3.LightSensor/Episode Code/LL.LightSensor/LL.LightSensor/ViewModels/DashboardViewModel.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
 
 namespace LL.LightSensor.ViewModels
 {
     public class DashboardViewModel : Metro.LL.Common.BaseViewModel
     {
+        private bool _isEventing;
+        private bool _isPolling;
+        private RelayCommand _togglePollingCommand;
+        private RelayCommand _toggleEventingCommand;
+
         public DashboardViewModel()
         {
             PageTitle = "Learning to use Light Sensor";
+        }
+
+        public RelayCommand TogglePollingCommand
+        {
+            get { return _togglePollingCommand ?? (_togglePollingCommand = new RelayCommand(TogglePolling)); }
+        }
+
+        private void TogglePolling()
+        {
+            IsPolling = !IsPolling;
+            IsEventing = false;
+        }
+
+        public RelayCommand ToggleEventingCommand
+        {
+            get { return _toggleEventingCommand ?? (_toggleEventingCommand = new RelayCommand(ToggleEventing)); }
+        }
+
+        private void ToggleEventing()
+        {
+            IsEventing = !IsEventing;
+            IsPolling = false;
+        }
+
+        public bool IsEventing
+        {
+            get { return _isEventing; }
+            set { _isEventing = value; OnPropertyChanged("IsEventing"); }
+        }
+
+        public bool IsPolling
+        {
+            get { return _isPolling; }
+            set { _isPolling = value; OnPropertyChanged("IsPolling"); }
         }
     }
 }
