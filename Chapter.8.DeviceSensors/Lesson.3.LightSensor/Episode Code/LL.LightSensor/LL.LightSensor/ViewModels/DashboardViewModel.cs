bd25753f6532abc91e6b7be98ce1f52d0f73e107
Derik Whittaker <derik@graudo.com>
2013-01-13 08:26:12 -0500

    WIP for light sensor

--- Chapter.8.DeviceSensors/Lesson.3.LightSensor/Episode Code/LL.LightSensor/LL.LightSensor/ViewModels/DashboardViewModel.cs
+++ Chapter.8.DeviceSensors/Lesson.3.LightSensor/Episode Code/LL.LightSensor/LL.LightSensor/ViewModels/DashboardViewModel.cs
@@ -4,19 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight.Command;
+using Sensor = Windows.Devices.Sensors;
 
 namespace LL.LightSensor.ViewModels
 {
     public class DashboardViewModel : Metro.LL.Common.BaseViewModel
     {
+        private Sensor.LightSensor _lightSensor;
+
         private bool _isEventing;
         private bool _isPolling;
         private RelayCommand _togglePollingCommand;
         private RelayCommand _toggleEventingCommand;
+        private double _brightness = .1;
+        private string _luxLums;
 
         public DashboardViewModel()
         {
             PageTitle = "Learning to use Light Sensor";
+            
+            SetupLightSensor();
+        }
+
+        private void SetupLightSensor()
+        {
+            _lightSensor = Sensor.LightSensor.GetDefault();
+
+            if (_lightSensor == null)
+            {
+                LuxLums = "Your current device does not suppor the light sensor";
+            }
+
         }
 
         public RelayCommand TogglePollingCommand
@@ -51,6 +69,18 @@ namespace LL.LightSensor.ViewModels
         {
             get { return _isPolling; }
             set { _isPolling = value; OnPropertyChanged("IsPolling"); }
+        }
+
+        public double Brightness
+        {
+            get { return _brightness; }
+            set { _brightness = value; }
+        }
+
+        public string LuxLums
+        {
+            get { return _luxLums; }
+            set { _luxLums = value; }
         }
     }
 }
