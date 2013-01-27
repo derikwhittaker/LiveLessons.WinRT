fbf20e12cfa54504a5614a92b5f9ac8462348c1b
Derik Whittaker <derik@graudo.com>
2013-01-27 15:16:22 -0500

    Adding zips of all the source code

---
+++ Chapter.7.TransitionsAnimations/Lesson.1.VisualStateManager/Episode Code/LL.VSM/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LL.VSM.ViewModels
+{
+    public class DashboardViewModel : Metro.LL.Common.BaseViewModel
+    {
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use Camera";
+            
+        }
+
+    }
+}
---
+++ Chapter.7.TransitionsAnimations/Lesson.1.VisualStateManager/Episode Code/LL.VSM/Views/DashboardPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LL.VSM.ViewModels;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace LL.VSM.Views
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class DashboardPage : LL.VSM.Common.LayoutAwarePage
+    {
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new DashboardViewModel();
+        }
+    }
+}
