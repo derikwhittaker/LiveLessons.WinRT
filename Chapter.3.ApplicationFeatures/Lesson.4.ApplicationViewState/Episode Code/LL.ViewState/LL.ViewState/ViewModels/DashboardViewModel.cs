cbfd4c5e85d10ff4a705f7052b6c51c73decd872
Derik Whittaker <derik@graudo.com>
2013-01-26 20:32:01 -0500

    WIP for viewstate

--- Chapter.3.ApplicationFeatures/Lesson.4.ApplicationViewState/Episode Code/LL.ViewState/LL.ViewState/ViewModels/DashboardViewModel.cs
+++ Chapter.3.ApplicationFeatures/Lesson.4.ApplicationViewState/Episode Code/LL.ViewState/LL.ViewState/ViewModels/DashboardViewModel.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LL.ViewState.DataModel;
+using Metro.LL.Common.Models;
 
 namespace LL.ViewState.ViewModels
 {
     public class DashboardViewModel : Metro.LL.Common.BaseViewModel
     {
+        private Repository _repository = new Repository();
+        private ObservableCollection<SimpleItem> _items;
+
         public DashboardViewModel()
         {
             PageTitle = "Learning to use View State";
+
+            Items = new ObservableCollection<SimpleItem>(_repository.All());
+        }
+
+        public ObservableCollection<SimpleItem> Items
+        {
+            get { return _items; }
+            set { _items = value; OnPropertyChanged("Items"); }
         }
     }
 }
