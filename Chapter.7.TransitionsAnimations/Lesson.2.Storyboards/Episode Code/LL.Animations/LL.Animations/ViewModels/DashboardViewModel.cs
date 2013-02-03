eb31958fbb0b9e58172bdd00f00a8fc3cb023203
Derik Whittaker <derik@graudo.com>
2013-02-03 09:18:11 -0500

    WIP for animations

---
+++ Chapter.7.TransitionsAnimations/Lesson.2.Storyboards/Episode Code/LL.Animations/LL.Animations/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight.Command;
+
+namespace LL.Animations.ViewModels
+{
+    public class DashboardViewModel : Metro.LL.Common.BaseViewModel
+    {
+        private RelayCommand _bounceBallCommand;
+        private RelayCommand _smoothBounceBallCommand;
+        private RelayCommand _easingBounceBallCommand;
+
+        public DashboardViewModel()
+        {
+            PageTitle = "Learning to use Animations";
+            
+        }
+
+        public RelayCommand BounceBallCommand
+        {
+            get { return _bounceBallCommand ?? (_bounceBallCommand = new RelayCommand(BounceBall)); }
+        }
+
+        private void BounceBall()
+        {
+            OnPropertyChanged("BounceBall");
+        }
+
+        public RelayCommand SmoothBounceBallCommand
+        {
+            get { return _smoothBounceBallCommand ?? (_smoothBounceBallCommand = new RelayCommand(SmoothBounceBall)); }
+        }
+
+        private void SmoothBounceBall()
+        {
+            OnPropertyChanged("SmoothBounceBall");
+        }
+
+        public RelayCommand EasingBounceBallCommand
+        {
+            get { return _easingBounceBallCommand ?? (_easingBounceBallCommand = new RelayCommand(EasingBounceBall)); }
+        }
+
+        private void EasingBounceBall()
+        {
+            OnPropertyChanged("EasingBounceBall");
+        }
+    }
+}
---
+++ Chapter.7.TransitionsAnimations/Lesson.2.Storyboards/Episode Code/LL.Animations/LL.Animations/Views/DashboardPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using LL.Animations.ViewModels;
+using Windows.UI.Xaml.Media.Animation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace LL.Animations.Views
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class DashboardPage : LL.Animations.Common.LayoutAwarePage
+    {
+
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+
+            var vm = new DashboardViewModel();
+
+            vm.PropertyChanged += OnPropertyChanged;
+
+            DataContext = vm;
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            switch (propertyChangedEventArgs.PropertyName)
+            {
+                case "BounceBall":
+                    BounceBallStoryboard.Begin();
+                    break;
+                case "SmoothBounceBall":
+                    SmoothBouceBallStoryboard.RepeatBehavior = new RepeatBehavior{Type = RepeatBehaviorType.Forever};
+                    SmoothBouceBallStoryboard.Begin();
+                    break;
+
+                case "EasingBounceBall":
+                    EasingAnimation.EasingFunction = new BounceEase
+                        {
+                            EasingMode = EasingMode.EaseIn
+                        };
+                    
+                    EasingBouceBallStoryboard.Begin();
+                    break;
+            }
+        }
+    }
+}
